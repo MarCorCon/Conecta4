a3da1a8e1fb6807eae63b70ba797b95f9356c086
MarcoCorzettoConflan <m_conflan@hotmail.it>
2017-05-12 05:39:49 +0200

    Intento de tiempo real

--- Connect4/src/main/java/es/upo/connect4/PopUpGame.java
+++ Connect4/src/main/java/es/upo/connect4/PopUpGame.java
@@ -5,24 +5,63 @@
  */
 package es.upo.connect4;
 
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.HorizontalLayout;
 
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import es.upo.connect4.Database.Match;
+import es.upo.connect4.Database.MongoClientHelper;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Marco
  */
 
-public class PopUpGame extends UI{
-        @Override
+public class PopUpGame extends UI implements Runnable{
+       private  Tablero t;
+       Match m;
+       VerticalLayout v = new VerticalLayout();
+       
+    @Override
     protected void init(VaadinRequest request) {
-            Tablero t = new Tablero(request.getParameter("u1"),request.getParameter("u2") );
-               this.setContent(  t.newTablero());
+            this.setImmediate(true);
+
+            t = new Tablero(request.getParameter("u1"),request.getParameter("u2") );
+            m = t.getMatch();
+            this.setImmediate(true);
+            v.addComponent(t.newTablero()); 
+            v.setImmediate(true);
+            this.setContent(  v);
+            (new Thread(this)).start();
+
 
 
                
     }
 
-  
-}
+         
+     @Override
+    public void run() {
+        while(m.getTurn()<42){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(PopUpGame.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            int turn = m.getTurn();
+            Match match = MongoClientHelper.getMatch(m.getId());
+            if(match.getTurn()>turn){
+            v.removeAllComponents();
+
+            v.addComponent(t.newTablero()); 
+            v.setImmediate(true);
+            
+            }
+        }
+    }
+
+  }
