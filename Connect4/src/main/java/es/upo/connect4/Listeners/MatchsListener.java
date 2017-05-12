6f2fdcbaac8cb370835ff4aa3283b3f0f21b843d
8418128 <segio91@gmail.com>
2017-05-12 09:15:19 +0200

    Update MatchsListener.java

--- Connect4/src/main/java/es/upo/connect4/Listeners/MatchsListener.java
+++ Connect4/src/main/java/es/upo/connect4/Listeners/MatchsListener.java
@@ -13,14 +13,26 @@ import com.vaadin.ui.UI;
  * @author S
  */
 public class MatchsListener extends AbstractListener{
+    Tablero t;
+    Match m;
+    VerticalLayout v;
     
-    public MatchsListener(UI ui,String otherUser){
+    public MatchsListener(UI ui,String otherUser,Tablero t, Match m, VerticalLayout v){
         this.ui=ui;
+        this.t=t;
+        this.m=m;
+        this.v=v;
     }
 
     @Override
     public void doWork() {
-        
+        int turn = m.getTurn();
+        Match match = MongoClientHelper.getMatch(m.getId());
+        if(match.getTurn()>turn){
+            v.removeAllComponents();
+            v.addComponent(t.newTablero()); 
+            v.setImmediate(true);   
+        }
     }
     
 }
